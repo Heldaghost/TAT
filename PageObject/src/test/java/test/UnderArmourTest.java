6c15fa844a8fed303e957d7d3ccc0d0092c0f35c
Sasha Prudilko <sprudilko01@gmail.com>
2020-12-09 23:20:27 +0300

    Fix conf prop

--- PageObject/src/test/java/test/UnderArmourTest.java
+++ PageObject/src/test/java/test/UnderArmourTest.java
@@ -3,12 +3,14 @@ package test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.annotations.AfterMethod;
 import page.*;
+import property.*;
 
 import java.util.List;
 
@@ -20,7 +22,12 @@ public class UnderArmourTest {
 
     @BeforeTest
     public void browserSetup() {
-        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", ConfigurationProperties.getProperty("chromedriver"));
+
+        ChromeOptions options = new ChromeOptions();
+
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
